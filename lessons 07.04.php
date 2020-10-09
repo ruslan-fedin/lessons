25449f1ec921ae0a2f051668f6d64ec46d70a0e7
ruslan-fedin <dbj7Psec>
2020-10-09 12:44:39 +0300

    listings 04.73

--- lessons 07.04.php
+++ lessons 07.04.php
@@ -1,4 +1,29 @@
 <?php
 
-echo "privet";
+class Person {
+    private $_name;
+    private $_age;
+
+    public static function __set($property, $value)
+    {
+        $method = "set{$property}";
+        if (method_exists($this, $method))
+        {
+            return $this->$method($value);
+        }
+    }
+
+    public function setName($name)
+    {
+        $this->_name =$name;
+        if( ! is_null($name)){
+            $this->_name=mb_strtoupper($this->_name);
+        }
+    }
+
+    public function setAge($age)
+    {
+        $this->_age=$age;
+    }
+}
 
